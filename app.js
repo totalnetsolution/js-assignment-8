6d64bb3694017a8291cdc56e1d87d78dc6d65c2e
muhammad-danish <totalnetsolution2@gmail.com>
2024-03-01 04:24:52 +0500

    index file created

---
+++ app.js
@@ -0,0 +1,68 @@
+var num1 = +prompt("Enter first number");
+var num2 = +prompt("Enter second number");
+var num3 = +prompt("Enter third number");
+var num4 = +prompt("Enter forth number");
+
+console.log (num1 + num2 + num3 + num4);
+
+
+// arthmatic opraters
+var Num1 = 10;
+var Num2 = 5;
+
+console.log(Num1 + Num2);
+console.log(Num1 - Num2);
+console.log(Num1 * Num2);
+console.log(Num1 / Num2);
+console.log(Num1 % Num2);
+
+// assign opraters
+var nUm1 =10;
+nUm1 = 20;
+console.log(nUm1); 
+
+// reassign opraters
+// var nUm1 =10;
+// nUm1 = nUm1 = 20;
+// nUm1 += 20;
+// nUm1 -= 20;
+// nUm1 *= 20;
+// nUm1 /= 20;
+// nUm1 %= 20;
+
+// console.log(nUm1); 
+
+// reassign opraters
+var nUm1 =10;
+nUm1 = nUm1 + 20;
+console.log(nUm1); 
+
+// Compression opraters
+
+console.log(10 > 5);
+console.log(10 < 5);
+console.log(10 <= 10);
+console.log(10 == 10);
+console.log(10 == "10");
+console.log(10 === "10");
+
+
+console.log(10 != 10);
+console.log(10 != "10");
+console.log(10 !== "10");
+
+
+var userName = "JavaScript";
+document.write ("<h1> + userName + </h1>")
+
+
+// logical opreater
+
+//&& == and
+//||  == OR
+
+var Compression = 10 <= 10 || 10 < 6 || 10 > 15;
+console.log(Compression)
+
+var Compression =10 <= 10 && 10 < 6 && 10 > 15;
+console.log(Compression);
